11c45d287d95d6c7df1a6b3db7ecc2edd0f7bab4
Kim Yen <tingyukimyen973@gmail.com>
2022-01-22 14:52:58 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created(){
+    this.axios.get('https://randomuser.me/api').then((res)=>{
+      console.log('get it!!!',res);
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+@import './assets/main';
+
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
